5464d313c28820d5b9b55b086f6114f645f54f40
Jacqueline Ryan <Jacqueline.Ryan@jpl.nasa.gov>
2025-01-27 09:35:56 -0800

    Fixed linter issues

--- src/vic2png/convert.py
+++ src/vic2png/convert.py
@@ -1,34 +1,33 @@
 """
-APACHE LICENSE, VERSION 2.0
-Copyright (c) 2023-24 California Institute of Technology (“Caltech”).
+Copyright (c) 2023-25 California Institute of Technology (“Caltech”).
 U.S. Government sponsorship acknowledged.
 
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are
 permitted provided that the following conditions are met:
-• Redistributions of source code must retain the above copyright notice, this list of
-  conditions and the following disclaimer.
-• Redistributions in binary form must reproduce the above copyright notice, this list
-  of conditions and the following disclaimer in the documentation and/or other materials
-  provided with the distribution.
-• Neither the name of Caltech nor its operating division, the Jet Propulsion Laboratory,
-  nor the names of its contributors may be used to endorse or promote products derived 
-  from this software without specific prior written permission.
+1. Redistributions of source code must retain the above copyright notice, this list of
+   conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list
+   of conditions and the following disclaimer in the documentation and/or other
+   materials provided with the distribution.
+3. Neither the name of Caltech nor its operating division, the Jet Propulsion
+   Laboratory, nor the names of its contributors may be used to endorse or promote
+   products derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
-THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
-OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
 LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import numpy as np
-from pathlib import Path, PurePath
+from pathlib import Path
 from PIL import Image
 import numpy.typing as npt
 from typing import Dict, FrozenSet, Optional, Tuple, Union
@@ -164,9 +163,9 @@ def vic2png(
     :param verbose: If True, print parsed information about the image.
     :return:        Location of the output image file.
     """
-    if type(source) != Path:
+    if type(source) is not Path:
         source = Path(source)
-    if out is not None and type(out) != Path:
+    if out is not None and type(out) is not Path:
         out = Path(out)
     if not fmt.startswith("."):
         fmt = "." + fmt
--- src/vic2png/reader.py
+++ src/vic2png/reader.py
@@ -1,30 +1,29 @@
 """
-APACHE LICENSE, VERSION 2.0
-Copyright (c) 2023-24 California Institute of Technology (“Caltech”).
+Copyright (c) 2023-25 California Institute of Technology (“Caltech”).
 U.S. Government sponsorship acknowledged.
 
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are
 permitted provided that the following conditions are met:
-• Redistributions of source code must retain the above copyright notice, this list of
-  conditions and the following disclaimer.
-• Redistributions in binary form must reproduce the above copyright notice, this list
-  of conditions and the following disclaimer in the documentation and/or other materials
-  provided with the distribution.
-• Neither the name of Caltech nor its operating division, the Jet Propulsion Laboratory,
-  nor the names of its contributors may be used to endorse or promote products derived 
-  from this software without specific prior written permission.
+1. Redistributions of source code must retain the above copyright notice, this list of
+   conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list
+   of conditions and the following disclaimer in the documentation and/or other
+   materials provided with the distribution.
+3. Neither the name of Caltech nor its operating division, the Jet Propulsion
+   Laboratory, nor the names of its contributors may be used to endorse or promote
+   products derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
-THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
-OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
 LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 from dataclasses import dataclass
@@ -244,10 +243,10 @@ def read_vic(filepath: Path) -> Tuple[pvl.PVLModule, np.ndarray]:
         with open(filepath, "r") as f:
             try:
                 label = pvl.load(f)
-            except:
+            except Exception as e:
                 raise UnsupportedFileTypeError(
                     "unsupported file type encountered, only VIC or IMG are allowed."
-                )
+                ) from e
         if label.get("ODL_VERSION_ID") is not None:
             parms = get_odl_imageparms(label)
         else:
